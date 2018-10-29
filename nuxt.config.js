84d745ee909d679df1d2ad71f3552de4213fbb5e
Sergei Kornienko <qluqua@gmail.com>
2018-10-29 17:05:08 +0300

    svg-sprite

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,11 @@
-const pkg = require('./package')
-
 module.exports = {
   mode: 'universal',
   head: {
-    title: pkg.name,
+    title: '@title',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
