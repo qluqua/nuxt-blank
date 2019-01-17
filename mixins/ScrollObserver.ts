0b6fe3ae1571329582ab729eb6fdd5abb57eccdd
Sergei Kornienko <qluqua@gmail.com>
2019-01-17 12:31:52 +0300

    ScrollObserver performance fix

--- mixins/ScrollObserver.ts
+++ mixins/ScrollObserver.ts
@@ -13,16 +13,14 @@ interface ScrollObserverOptions {
    */
   elements: HTMLElement[],
   /**
-   * Имя CSS-класса который добавляется ко всем элементам вне видимой области
-   * и удаляется при их "появлении", таким образом анимируя переход от скрытого
+   * Имя CSS-класса который удаляется у всех элементов в видимой области
+   * таким образом анимируя переход от скрытого
    * состояния к видимому. Все анимации/транзишены описывайте в СSS.
-   * Значение по-умолчанию: 'is-scroll-invisible'
    */
   classAnimateFrom: string,
   /**
-   * Добавлять класс 'is-scroll-invisible' (или ваш кастомный) при исчезновении
+   * Добавлять класс 'js-soa-start' (или ваш кастомный) при исчезновении
    * элеементов, что бы анимировать их вновь при попадании в видимую область?
-   * Значение по-умолчанию: false
    */
   reverse?: boolean,
   /**
@@ -31,7 +29,6 @@ interface ScrollObserverOptions {
    * войдет в видимую область хотя бы на 50 пикселей. Так же элемент потеряет класс
    * видимого за 50 пикселей до своего выхода из видимой области, если исользовать
    * параметр reverse: true.
-   * Значение по-умолчанию: 50
    */
   offset?: number
 }
@@ -40,7 +37,7 @@ export default class ScrollObserver {
   private options: ScrollObserverOptions
   private optionsDefault: ScrollObserverOptions = {
     elements: null,
-    classAnimateFrom: 'is-scroll-invisible',
+    classAnimateFrom: 'js-soa-start',
     offset: 50,
     reverse: false
   }
@@ -53,20 +50,24 @@ export default class ScrollObserver {
 
   private initObersver() {
     const observer = new IntersectionObserver(entries => {
-      for (const entry of entries) {
-        const { isIntersecting, target } = entry
-        const className = this.options.classAnimateFrom
-        const reverse = this.options.reverse
+      try {
+        for (const entry of entries) {
+          const { isIntersecting, target } = entry
+          const className = this.options.classAnimateFrom
+          const reverse = this.options.reverse
 
-        if (isIntersecting) {
-          target.classList.remove(className)
+          if (isIntersecting) {
+            target.classList.remove(className)
 
-          if (!reverse) {
-            observer.unobserve(target)
+            if (!reverse) {
+              observer.unobserve(target)
+            }
+          } else {
+            target.classList.add(className)
           }
-        } else {
-          target.classList.add(className)
         }
+      } catch (error) {
+        console.log('ScrollObserver: ' + error)
       }
     }, {
       // IntersectionObserver API options:
@@ -75,9 +76,18 @@ export default class ScrollObserver {
       // threshold: 0
     })
 
-    for (const el of this.options.elements) {
-      observer.observe(el)
-      el.classList.add(this.options.classAnimateFrom)
+    const elements = this.options.elements
+
+    if (!elements || !elements.length) {
+      return
+    }
+
+    try {
+      for (const el of elements) {
+        observer.observe(el)
+      }
+    } catch (error) {
+      console.log('ScrollObserver: ' + error)
     }
   }
 }
