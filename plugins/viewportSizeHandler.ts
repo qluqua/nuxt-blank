b9fcf31dcbd86a161bd45467e9ea407c3507d38b
Sergey Korepanov <38503175+svkorepanov@users.noreply.github.com>
2019-03-07 17:11:32 +0300

    Поправил ошибку viewportSizeHandler.ts
    Высота документа не равняется высоте экрана.

--- plugins/viewportSizeHandler.ts
+++ plugins/viewportSizeHandler.ts
@@ -5,10 +5,11 @@ export default ({ store }) => {
     const queriesAll = Object.entries(store.state.ui.grid.queries)
     const queriesCurrent = queriesAll.filter((query: any) => window.matchMedia(query[1]).matches)
     const breakpoint = queriesCurrent.find(rule => rule[0].length === 2)[0]
-    const { offsetWidth, offsetHeight } = document.documentElement
+    const { offsetWidth } = document.documentElement
+    const windowHeight = window.innerHeight
     const scrollbarWidth = window.innerWidth - offsetWidth
 
-    store.commit('ui/updateViewportInfo', [offsetWidth, offsetHeight, breakpoint, scrollbarWidth])
+    store.commit('ui/updateViewportInfo', [offsetWidth, windowHeight, breakpoint, scrollbarWidth])
   }
 
   viewportSizeHandler()
