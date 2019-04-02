9f20b599a28d79ef1cc937075d30fc290f42ad0f
Sergei Kornienko <kornienko@beta.agency>
2019-04-02 18:42:20 +0300

    iframe resize handler

--- plugins/viewportSizeHandler.ts
+++ plugins/viewportSizeHandler.ts
@@ -1,19 +1,31 @@
-// import { debounce } from 'lodash-es'
+import { debounce } from 'lodash-es'
 
 export default ({ store }) => {
-  const viewportSizeHandler = () => {
+  // Resize. The reason for iframe usage is window resize event, that doesn't fire if
+  // vertical scrollbar appears and this causes problems time after time
+  const iframe = document.createElement('iframe')
+
+  iframe.setAttribute('id', '_resizeTrigger')
+  iframe.setAttribute('style', 'position: fixed; z-index: -9999; visibility: hidden; top: -1px; left: 0; right: 0; height: 0px; width: 100%; border: none;')
+  document.body.appendChild(iframe)
+  iframe.contentWindow.addEventListener('resize', sizeHandler)
+
+  function sizeHandler() {
     const queriesAll = Object.entries(store.state.ui.grid.queries)
     const queriesCurrent = queriesAll.filter((query: any) => window.matchMedia(query[1]).matches)
     const breakpoint = queriesCurrent.find(rule => rule[0].length === 2)[0]
     const { offsetWidth } = document.documentElement
-    const windowHeight = window.innerHeight
     const scrollbarWidth = window.innerWidth - offsetWidth
 
-    store.commit('ui/updateViewportInfo', [offsetWidth, windowHeight, breakpoint, scrollbarWidth])
+    store.commit('ui/updateViewportInfo', [offsetWidth, window.innerHeight, breakpoint, scrollbarWidth])
   }
 
-  viewportSizeHandler()
+  sizeHandler()
 
-  window.addEventListener('resize', viewportSizeHandler)
-  // window.addEventListener('resize', debounce(viewportSizeHandler, 60))
+  // Scroll
+  const scrollHandler = () => {
+    store.commit('ui/setScrollPosition', window.pageYOffset)
+  }
+
+  window.addEventListener('scroll', debounce(scrollHandler, 16))
 }
