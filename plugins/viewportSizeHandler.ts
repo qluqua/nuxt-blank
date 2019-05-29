64ace8e2660195acce8b4a977c92108171e9f96f
Sergei Kornienko <kornienko@beta.agency>
2019-05-29 18:07:18 +0300

    fix(viewportHandler): styles fix

--- plugins/viewportSizeHandler.ts
+++ plugins/viewportSizeHandler.ts
@@ -16,7 +16,7 @@ export default ({ store }) => {
   const iframe = document.createElement('iframe')
 
   iframe.setAttribute('id', '_resizeTrigger')
-  iframe.setAttribute('style', 'position: fixed; z-index: -9999; visibility: hidden; top: -1px; left: 0; right: 0; height: 0px; width: 100%; border: none; min-height: 100vh;')
+  iframe.setAttribute('style', 'position: fixed; z-index: -9999; visibility: hidden; width: 100%; height: 100%; border: none;')
   document.body.appendChild(iframe)
   iframe.contentWindow.addEventListener('resize', sizeHandler)
 
