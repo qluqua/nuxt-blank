529fd0d3c317572397f9fd465d5a5f80fbc1dcad
Sergei Kornienko <Sergei.Kornienko@moex.com>
2020-03-17 17:03:53 +0300

    WIP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -97,18 +97,6 @@ export default {
         useShortDoctype: false
       }
     },
-    babel: {
-      plugins: [
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        [
-          '@babel/plugin-proposal-pipeline-operator',
-          {
-            proposal: 'minimal',
-          },
-        ],
-      ],
-    },
   },
   // sitemap: {
   //   path: '/sitemap.xml',
--- utils/server.js
+++
@@ -1,3 +0,0 @@
-export const isDevelop = process.env.NODE_ENV === 'development';
-export const isProduction = process.env.NODE_ENV === 'production';
-export const isProductionStand = process.env.STAND === 'production';
